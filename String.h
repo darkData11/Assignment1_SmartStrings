42a7cd68b9082093eae2b6956e57be37cf580203
darkData11 <hassaant0@gmail.com>
2016-03-05 23:46:41 +0500

    Create String.h

---
+++ String.h
@@ -0,0 +1,20 @@
+#ifndef STRING_H
+#define	STRING_H
+
+class String{
+private:
+	StringBuffer* _str;
+public:
+	String();
+	~String();
+	String(String&);
+	String(char*, int);
+	void append(char);
+	int length() const;
+	char charAt(int) const;
+	bool owner;
+};
+
+
+#endif	/* STRING_H */
+
